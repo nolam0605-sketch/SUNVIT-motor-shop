c744073fe827d586389a8463bb085cfdebb19eaf
nolam0605-sketch <nolam0605@gmail.com>
2026-01-30 11:29:36 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const motorcycleParts = [
+    {
+        name: "Brake Pad",
+        price: 500,
+        category: "Brakes",
+        image: "images/brake-pad.jpg" // Upload your image to an 'images' folder in the repo
+    },
+    {
+        name: "Chain",
+        price: 800,
+        category: "Transmission",
+        image: "images/chain.jpg"
+    },
+    // Add more items here...
+    // Example: { name: "Tire", price: 1500, category: "Wheels", image: "images/tire.jpg" }
+];
